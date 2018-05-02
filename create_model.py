89e455ce0f9c265f036a81d1f191e2680949eddb
Diego Amicabile <diego.amicabile@gmail.com>
2018-05-02 04:48:45 +0200

    Doing doc2vec

--- create_model.py
+++ create_model.py
@@ -5,7 +5,7 @@ from repository.mongo_ops import iterate_mod_questions_in_mongo
 
 from bd_gensim.gram_facade import GramFacade
 from bd_gensim.doc2vec_facade import Doc2VecFacade
-
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 if __name__ == '__main__':
 
     config = yaml.safe_load(open("config.yml"))
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     models_dir = config['models_dir']
 
     mongo_connection = config['mongo_connection']
-    gramFacade = GramFacade(models_dir,10,10)
+    gramFacade = GramFacade(models_dir,8,8)
     all_questions = list(iterate_mod_questions_in_mongo(mongo_connection, separator=False))
     gramFacade.create_model(all_questions )
 
@@ -24,5 +24,5 @@ if __name__ == '__main__':
     with open(data_dir+"/trigrams.txt","w") as f:
         print(trigrams, file=f)
 
-    doc2vecFacade = Doc2VecFacade(models_dir, window=7, min_count=3, sample=0.001, epochs=80, alpha=0.1, vector_size=400, batch_size=10000)
+    doc2vecFacade = Doc2VecFacade(models_dir, window=8, min_count=4, sample=0.001, epochs=200, alpha=0.1, vector_size=300, batch_size=10000)
     doc2vecFacade.create_model(trigrams)
